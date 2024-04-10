0e997b573a0c60f60756723b6c05f17da4c535b6
genesis-boop <genesisgithub@gmail.com>
2024-04-09 23:04:02 -0700

    Added data files and updated endpoints for API

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-const todoRoutes = require("./routes/todoRoutes");
 const cors = require("cors");
+const fs = require('fs/promises');
+const uniqid = require('uniqid');
 
 // Middleware
 app.use(cors());
@@ -13,7 +14,25 @@ app.use((_req, _res, next) => {
     next();
 })
 
-app.use("/todo", todoRoutes);
+// API endpoint to save tasks
+app.post('/todo', async (req, res) => {
+    try{
+        const newTask = req.body;
+        newTask.id = uniqid();
+
+        const taskData = await fs.readFile('./data/todo.json', 'utf-8');
+        const tasks = JSON.parse(taskData);
+        tasks.push(newTask);
+
+        await fs.writeFile('./data/todo.json', JSON.stringify(tasks));
+        res.status(201).json({message: 'Task Created!'});
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({message: 'Failed to save task'});
+    }
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
